2f2e2aa7cdea5e799ccf6cdedb960589fdf274b2
Voicu Dordea <d0rdea2@gmail.com>
2022-12-02 15:04:42 +0200

    changed game logic

--- MultipleLevelsGame.playground/Contents.swift
+++ MultipleLevelsGame.playground/Contents.swift
@@ -4,16 +4,14 @@ let levels = 10
 let freeLevels = 4
 let bonusLevel = 3
 
-for i in 1...levels {
-    if i == bonusLevel {
-        continue
+for level in 1...levels {
+    if level == bonusLevel {
         print("Skip bonus level \(bonusLevel).")
+        continue
     }
-    
-    if i <= freeLevels {
-        print("Play free level \(i).")
-    } else {
+    print("Play level \(level).")
+    if level == freeLevels {
+        print("You have played all \(freeLevels) free levels. Buy the game to play the remaining \(levels - freeLevels) levels.")
         break
-        print("Play paid level \(i).")
     }
 }
