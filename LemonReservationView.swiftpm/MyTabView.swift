fa476cae6cc24abce47fee5b0b6c9939fe6c307d
Voicu Dordea <d0rdea2@gmail.com>
2022-12-10 09:20:15 +0200

    changes

--- LemonReservationView.swiftpm/MyTabView.swift
+++ LemonReservationView.swiftpm/MyTabView.swift
@@ -11,13 +11,13 @@ struct MyTabView: View {
     var body: some View {
         VStack {
             TabView {
+                
                 Text("This is the Share View")
-                    .font(.title)
                     .tabItem {
                         Label("Share", systemImage: "square.and.arrow.up")
                     }
+                
                 Text("This is the Trash View")
-                    .font(.title)
                     .tabItem {
                         Label("Trash", systemImage: "trash")
                     }
