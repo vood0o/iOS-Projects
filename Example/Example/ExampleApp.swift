cf6ae0b163b842a93422de9aebdd1697eca84409
Voicu Dordea <d0rdea2@gmail.com>
2022-12-15 14:49:24 +0200

    exampleApp

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Voicu on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
